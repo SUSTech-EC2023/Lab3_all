b05578217e19600d5c23400111ce5dad0e06afdc
ZHANG Han <56686001+HanZhang-ZERO@users.noreply.github.com>
2023-02-26 01:17:31 +0800

    Update exponential_fitness_scaling.m

--- exponential_fitness_scaling.m
+++ exponential_fitness_scaling.m
@@ -5,5 +5,5 @@ end
 mu = length(fitness_list);
 exponential_fitness_list = nan(mu,1);
 for i = 1:mu
-    exponential_fitness_list(i) = exp(fitness_list(i/T);
+    exponential_fitness_list(i) = exp(fitness_list(i/T));
 end
